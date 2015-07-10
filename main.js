8aac4194ab016deb38d436fd87e145401290df88
Luis <delapenl@gmail.com>
2015-07-10 16:50:03 -0600

    added index html and main.js for javascript loops

---
+++ main.js
@@ -0,0 +1,32 @@
+// Problem 1 //
+for (i = 0; i <= 100; i+=10) {
+  console.log(i);
+}
+//Problem 2 //
+for (i = 1; i <= 128; i*=2) {
+  console.log(i);
+}
+//Problem 3 //
+for (i = 0; i <=10; i +=2){
+    console.log(i);
+}
+//Problem 4 //
+for (i = 3; i <=15; i +=3){
+    console.log(i);
+}
+//Problem 5 //
+for (i = 9; i >=0; i -=1){
+    console.log(i);
+}
+//Problem 6 //
+for (var i = 1; i <=4 ; i++ ) {
+    for (var j = 1; j <= 3; j++) {
+        console.log(i);
+}
+}
+//Problem 7 //
+for (var i = 0; i <=2 ; i++ ) {
+    for (var j = 0; j <= 4; j++) {
+        console.log(j);
+}
+}
